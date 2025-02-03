8b2160ed9bb64cf7522c996e2e4acc0d9e9b3500
nazarinnouhad <nazarinnazarin2002@gmail.com>
2025-02-03 11:23:08 +0530

    Local storage,Date,State

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Todo from './components/Todo';
+const localData = ()=>{
+  let list = localStorage.getItem("data")
+  if (list){
+    return JSON.parse(list)
+  }else{
+    return[]
+  }
+}
 function App() {
-  const [todoList, settodoList] = useState([
-    {id :1 , taskName : "Sleep", iscomplete : false},
-    {id :2 , taskName : "Go to Gym", iscomplete : false},
-    {id :3 , taskName : "Eat Breakfast", iscomplete : false},
-    {id :4 , taskName : "Study", iscomplete : false}
-     ]); 
+  const [todoList, settodoList] = useState(localData()); 
      const [newTask, setnewTask] = useState("");
-
+     const [date, setDate] = useState(new Date());
+const clock = ()=>{
+  setInterval(()=>{setDate(new Date(),1000)})
+}
+useEffect(()=>{
+  clock()
+  return function clearUp(){
+    clearInterval(clock)
+  }
+},[])
      // Add Task
 
  const addTask =(e)=>{ 
@@ -18,11 +30,18 @@ function App() {
  }
  else{
   let num = todoList.length +1
-  let newEntry = {id:num,taskName:newTask,iscomplete:false}
+  let newEntry = {id:num,taskName:newTask,isComplete:false}
   settodoList([...todoList,newEntry])
   setnewTask("") 
  } 
 }
+
+// local storage
+
+useEffect(()=>{
+  localStorage.setItem("data",JSON.stringify(todoList))
+},[todoList])
+
 // Delete Task
 
 const deleteTask = (id) => {
@@ -31,9 +50,21 @@ const deleteTask = (id) => {
     return item.id !==id
   }))
 }
+
+// iscomplete
+
+const doneTask = (id) => {
+  console.log("working")
+  settodoList(todoList.map((item) => {
+    return item.id === id ? { ...item, isComplete: !item.iscomplete } : item
+  }))
+  
+}
   return (
 <div className="p-4 min-h-screen flex flex-col  items-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% pt-28">
       <div className="font-bold text-3xl">TO-Do</div>
+      <h2 className='mt-10 font-bold text-lg text-pink-900 font-serif'>Time: {date.toLocaleTimeString()}</h2>
+      <h2 className='mt- font-bold text-md font-serif text-pink-900'>Date: {date.toLocaleDateString()}</h2>
       <form className="flex gap-5 mt-10">
         <input value={newTask} onChange={(e)=>{setnewTask(e.target.value)}} className=" border-2 border-white px-4 py-2  outline-none rounded-md " type="text" placeholder="Enter the Task" />
         <button onClick={addTask} className=" border-2 border-white rounded-md font-semibold bg-white text-sm p-2">
@@ -44,7 +75,9 @@ const deleteTask = (id) => {
         {
          todoList.map((item) => {
           return (
-            <Todo item ={item} key={item.id} deleteTask={deleteTask}/>
+            <Todo item ={item} key={item.id} deleteTask={deleteTask}
+            doneTask={doneTask}/>
+            
             
           )
          })  
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 import DoneIcon from '@mui/icons-material/Done';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-const Todo = ({ item, deleteTask }) => {
+const Todo = ({ item, deleteTask,doneTask }) => {
   return (
  <div className=" bg-black rounded-md h-36 w-56 text-center text-white text-md font-serif pt-4 ">
-      <h1>{item.id} . {item.taskName}</h1>
+      <h1>{item.id} . <span className= {`${item.isComplete ? "line-through text-green-500" : "text-white"}`}>{item.taskName}</span> </h1>
       <div className="pt-14">
-        <DoneIcon className="text-green-500 hover:text-green-700" />
+        <DoneIcon onClick = {() => {doneTask(item.id)}} className="text-green-500 hover:text-green-700" />
         <DeleteIcon
           onClick={() => deleteTask(item.id)}
           className="text-red-500 hover:text-red-700"
@@ -20,5 +20,6 @@ const Todo = ({ item, deleteTask }) => {
 Todo.propTypes = {
   item : PropTypes.item,
   deleteTask : PropTypes.deleteTask,
+  doneTask : PropTypes.doneTask,
 }
 export default Todo;
